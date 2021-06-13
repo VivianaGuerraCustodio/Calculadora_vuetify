1de5b036f55f205fc932c8e8a1e340aa2a76478a
viviana2397 <vivianaguerracustodio@gmail.com>
2021-06-13 10:51:55 -0500

    cambio de color en el gradiente

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
 	<v-app>
-		<v-app-bar app color="blue accent-3" >
+		<v-app-bar app color="blue accent-3">
 			<div class="d-flex align-center">
 				<v-img
 					alt="Vuetify Name"
@@ -19,7 +19,7 @@
 			<v-btn
 				href="https://github.com/VivianaGuerraCustodio"
 				target="_blank"
-        text
+				text
 			>
 				<v-img
 					alt="logo de github"
@@ -52,14 +52,15 @@
 	};
 </script>
 <style lang="scss" scoped>
-	html, #app {
+	html,
+	#app {
 		background: radial-gradient(
 				41.48% 11.48% at 2.38% 8.52%,
-				#747de9 0%,
+				#8a8a8e 0%,
 				rgba(255, 255, 255, 0) 100%
 			),
-			linear-gradient(135deg, #72edf2 0%, #5151e5 100%);
-      overflow: hidden!important;
-      overflow-y: hidden!important;
+			linear-gradient(135deg, #37e7ee 0%, #111133 100%);
+		overflow: hidden !important;
+		overflow-y: hidden !important;
 	}
 </style>
